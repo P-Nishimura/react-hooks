5f7893db51d7aca8d2d87b9e2abd4a7ad2acab1c
Yuki Nishimura <pyn_consoles1021@icloud.com>
2020-03-09 00:48:23 +0900

    imprement object state.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React, { useState } from 'react';
 
 const App = (props) => {
-  const initialState = props;
-  const [name, setName] = useState(initialState.name);
-  const [price, setPrice] = useState(initialState.price);
-
-  const reset = () => {
-    setPrice(props.price);
-    setName(props.name);
-  };
+  const [state, setState] = useState(props);
+  const { name, price } = state;
 
   return (
     <>
-      <p>現在の{name}は{price}です。</p>
-      <button onClick={() => {setPrice(price + 1)}}>+1</button>
-      <button onClick={() => {setPrice(price - 1)}}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <input value={name} onChange={e => setName(e.target.value)} />
+      <p>現在の{name}は{price}円です。</p>
+      <button onClick={() => {setState({...state, price: price + 1})}}>+1</button>
+      <button onClick={() => {setState({...state, price: price - 1})}}>-1</button>
+      <button onClick={() => {setState(props)}}>Reset</button>
+      <input value={name} onChange={e => setState({...state, name: e.target.value})} />
     </>
   );
 }
 
 App.defaultProps = {
-  name: 'Nishimura',
+  name: 'KAIMAN',
   price: 1000,
 };
 
