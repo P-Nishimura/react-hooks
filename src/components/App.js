7b7fb93fedf65469b527c913ab89e837efa191e8
Yuki Nishimura <pyn_consoles1021@icloud.com>
2020-03-11 01:22:37 +0900

    refactore components.

--- src/components/App.js
+++ src/components/App.js
@@ -1,67 +1,17 @@
-import React, { useReducer, useState } from 'react';
+import React, { useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Event from './Event';
+import EventForm from './EventForm';
+import Events from './Events';
 import reducer from '../reducers';
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-
-  const addEvent = (e) => {
-    e.preventDefault();
-
-    dispatch({
-      type: 'CREATE_EVENT',
-      title,
-      body,
-    });
-
-    setTitle('');
-    setBody('');
-  };
-
-  const deleteAllEvents = (e) => {
-    e.preventDefault();
-    const deleteCheck = window.confirm('イベントを全て削除してもよろしいですか？');
-    if(deleteCheck) dispatch({ type: "DELETE_ALL_EVENTS" });
-  };
-
-  const unCreatable = title === '' || body === '';
-  const unAllDeletable = state.length === 0;
 
   return (
     <div className="container-fluid">
-      <h4>Event Create Form</h4>
-      <form>
-        <div className="form-group">
-          <label htmlFor="formEventTitle">Title</label>
-          <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)}></input>
-        </div>
-        <div className="form-group">
-          <label htmlFor="formEventBody">Body</label>
-          <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)}></textarea>
-        </div>
-
-        <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>Event Create</button>
-        <button className="btn btn-danger" onClick={deleteAllEvents} disabled={unAllDeletable}>All Event Delete</button>
-
-        <h4>Event List</h4>
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Title</th>
-              <th>Body</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            { state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />)) }
-          </tbody>
-        </table>
-      </form>
+      <EventForm state={state} dispatch={dispatch} />
+      <Events state={state} dispatch={dispatch} />
     </div>
   );
 }
---
+++ src/components/EventForm.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const EventForm = (props) => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const addEvent = (e) => {
+    e.preventDefault();
+
+    props.dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body,
+    });
+
+    setTitle('');
+    setBody('');
+  };
+
+  const deleteAllEvents = (e) => {
+    e.preventDefault();
+    const deleteCheck = window.confirm('イベントを全て削除してもよろしいですか？');
+    if(deleteCheck) props.dispatch({ type: "DELETE_ALL_EVENTS" });
+  };
+
+  const unCreatable = title === '' || body === '';
+  const unAllDeletable = props.state.length === 0;
+
+  return (
+    <>
+      <h4>Event Create Form</h4>
+      <form>
+        <div className="form-group">
+          <label htmlFor="formEventTitle">Title</label>
+          <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)}></input>
+        </div>
+        <div className="form-group">
+          <label htmlFor="formEventBody">Body</label>
+          <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)}></textarea>
+        </div>
+
+        <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>Event Create</button>
+        <button className="btn btn-danger" onClick={deleteAllEvents} disabled={unAllDeletable}>All Event Delete</button>
+      </form>
+    </>
+  );
+}
+
+export default EventForm;
